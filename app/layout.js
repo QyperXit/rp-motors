bb1c4227b6c2a8e36a6190e637f56829a6483b97
QyperXit <67711154+QyperXit@users.noreply.github.com>
2025-01-30 00:07:32 +0000

    Added - Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,25 @@
 import { Inter } from "next/font/google";
 import { SearchContextProvider } from "./components/context/search";
 import "./globals.css";
+import GoogleAnalytics from "./util/analytics/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "R P Motors",
-  description: "Built by QyperXit",
+    title: "R P Motors",
+    description: "Built by QyperXit",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <SearchContextProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </SearchContextProvider>
-  );
+    return (
+        <SearchContextProvider>
+            <html lang="en">
+            <body className={inter.className}>
+            <GoogleAnalytics GA_MEASUREMENT_ID="G-KQCFWFMNRQ" />
+
+            {children}
+            </body>
+            </html>
+        </SearchContextProvider>
+    );
 }
